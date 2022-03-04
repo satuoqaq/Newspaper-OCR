122c75863c69167b5232ac16df665587878248cb
FZY <2788321369@qq.com>
2022-03-04 18:07:22 +0800

    修改了写入excel文件  加入了年份判定以及专业对应的页码数，图片数

--- P8_write_excel.py
+++ P8_write_excel.py
@@ -4,14 +4,35 @@ import xlwt
 from P0_init import conf
 
 
+def get_year(name, sch_batch):
+    if sch_batch == '专科':
+        return 3
+    if name.find('民航运输') != -1:
+        return 3
+    if name.find('7+X') != -1 or name.find('本硕连读') != -1:
+        return 7
+    if name.find('IX') != -1 or name.find('九年') != -1 or name.find('9年') != -1:
+        return 9
+    if name.find('Vm') != -1 or name.find('vm') != -1 or name.find('本硕博') != -1 or name.find('8年') != -1 or name.find(
+            '八年') != -1 or name.find('本博连读') != -1:
+        return 8
+    if name.find('Ⅱ') != -1 or name.find('[I]') != -1:
+        return 2
+    if name.find('医学') != -1 or name.find('V') != -1:
+        return 5
+    return 4
+
+
 def write_excel(baseRoot):
-    with open(os.path.join(baseRoot, 'json/School_msg.json'), 'r', encoding='UTF-8')as fp:
+    with open(os.path.join(baseRoot, 'json/School_msg.json'), 'r', encoding='UTF-8') as fp:
         sch_data = json.load(fp)
+    with open(os.path.join(baseRoot, 'json/page_msg.json'), 'r', encoding='UTF-8') as fp1:
+        page_data = json.load(fp1)
 
     # pro = ['招生代码', '学校名称', '计划人数', '检测计划人数', '图片页码', '学校备注']
     pro = ['年份', '高考省份', '科类', '录取批次', '招生代码', '学校名称', '计划人数', '检测计划人数', '学校所在页', '图片页码', '学校备注']
     # pro1 = ['招生代码', '学校名称', '专业代码', '专业名称', '计划人数', '学费', '专业备注']
-    pro1 = ['年份', '高考省份', '科类', '录取批次', '招生代码', '学校名称', '专业招生代码', '专业名称', '计划人数', '学制', '学费', '专业备注']
+    pro1 = ['年份', '高考省份', '科类', '录取批次', '招生代码', '学校名称', '专业招生代码', '专业名称', '计划人数', '学制', '学费', '报纸页码数','图片页码数', '专业备注']
     book = xlwt.Workbook()
     sheet = book.add_sheet('Sheet1')
     sheet2 = book.add_sheet('Sheet2')
@@ -45,14 +66,16 @@ def write_excel(baseRoot):
             sheet2.write(num, 6, maj['id'])
             sheet2.write(num, 7, maj['name'])
             sheet2.write(num, 8, int(maj['place']))
-            sheet2.write(num, 9, 4)
+            sheet2.write(num, 9, get_year(maj['name'], sch['batch']))
             sheet2.write(num, 10, maj['tuition'])
+            sheet2.write(num, 11, page_data[maj['page_name']]['page_number'])
+            sheet2.write(num, 12, maj['page_name'])
             if maj['place'] != '0' and len(maj['id']) == 2:
-                sheet2.write(num, 11, 'ac')
+                sheet2.write(num, 13, 'ac')
             elif maj['place'] == '0':
-                sheet2.write(num, 11, '未检测到专业名额')
+                sheet2.write(num, 13, '未检测到专业名额')
             else:
-                sheet2.write(num, 11, '专业名出错')
+                sheet2.write(num, 13, '专业名出错')
             sum_place = sum_place + int(maj['place'])
             num = num + 1
         sheet.write(i + 1, 7, sum_place)
@@ -67,7 +90,7 @@ def write_excel(baseRoot):
 
 
 def main():
-    write_excel('PC3')
+    write_excel('PC1')
 
 
 if __name__ == "__main__":
